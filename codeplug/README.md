12adda3f6bffa07fdaa3c825781fe7a450a53ac8
Masen Furer <m_github@0x26.net>
2021-01-13 05:43:33 -0800

    create separate codeplug builds

--- codeplug/README.md
+++ codeplug/README.md
@@ -1,3 +1,4 @@
-The codeplug directory contains files that
-may need to be customized to generate the desired
-codeplug.
+The codeplug directory contains directories that will be built into
+codeplug import files by the CI process.
+
+See default and default-tyt-md380 for more info. See kf7hvm for a customized codeplug.
---
+++ codeplug/kf7hvm/generate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+OUTPUT=${OUTPUT:OUTPUT}
+python -m dzcb \
+    --pnwdigital \
+    --seattledmr \
+    --default-k7abd \
+    --repeaterbook-proximity-csv $DIR/prox.sh \
+    --k7abd $DIR/k7abd \
+    --scanlists-json $DIR/scanlists.json
+    --order-json $DIR/order.json \
+    --farnsworth-template-json $DIR/md-uv380.json \
+$OUTPUT/kf7hvm
