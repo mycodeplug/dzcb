27a4b80a471825f2d2ffc94ea53f4420948f1fe9
Masen Furer <m_github@0x26.net>
2021-01-13 06:07:52 -0800

    move md380/390 templates to custom codeplug dir
    md380 is not generated in the "default" set, because it cannot accomodate
    all of the channels

--- codeplug/default-tyt-md380/generate.sh
+++ codeplug/default-tyt-md380/generate.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-OUTPUT=${OUTPUT:OUTPUT}
+OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
 python -m dzcb \
     --pnwdigital \
     --seattledmr \
     --default-k7abd \
     --scanlists-json $DIR/scanlists.json \
     --order-json $DIR/order.json \
+    --farnsworth-template-json $DIR/md380-uhf.json \
+    --farnsworth-template-json $DIR/md380-vhf.json \
+    --farnsworth-template-json $DIR/md390-uhf.json \
+    --farnsworth-template-json $DIR/md390-vhf.json \
 $OUTPUT/default-tyt-md380
