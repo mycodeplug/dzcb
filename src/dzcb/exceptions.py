11b2bb2bf5fba6b7fe3cc326f962ea21ecf54b01
Masen Furer <m_github@0x26.net>
2021-01-24 09:14:33 -0800

    dzcb.exceptions: DuplicateDmrID
    Raised when attempting to instantiate a Contact
    with an ID which has already been seen.
    
    Anytone 868 requires unique ID->Name mapping

--- src/dzcb/exceptions.py
+++ src/dzcb/exceptions.py
@@ -2,6 +2,12 @@
 dzcb.exceptions - errors that may be raised
 """
 
+
 class InvalidDmrID(ValueError):
     pass
 
+
+class DuplicateDmrID(ValueError):
+    def __init__(self, msg, existing_contact):
+        super().__init__(msg)
+        self.existing_contact = existing_contact
