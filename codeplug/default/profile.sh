e7eb4851f0fdcf71ca4752458438d4255c557bcf
Masen Furer <m_github@0x26.net>
2021-02-20 22:19:32 -0800

    default codeplug: update for 0.3
    use new CSV order format

--- codeplug/default/profile.sh
+++ codeplug/default/profile.sh
@@ -2,12 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
-python -m dzcb \
+python -m cProfile -o $OUTPUT/profile -m dzcb \
     --pnwdigital \
     --seattledmr \
     --default-k7abd \
     --repeaterbook-state washington oregon \
     --repeaterbook-proximity-csv "$DIR/../../src/dzcb/data/repeaterbook_proximity_zones.csv" \
     --scanlists-json $DIR/scanlists.json \
-    --order $DIR/order.csv -- \
+    --order order.csv -- \
 $OUTPUT/default
