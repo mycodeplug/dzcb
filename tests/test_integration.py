9085c4347b152f7fa77702c542188d4b5ced7835
Masen Furer <m_github@0x26.net>
2021-02-26 00:01:48 -0800

    test_integration: clean up diff check

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,7 @@
 """
 Integration tests combine multiple features
 """
+import difflib
 from filecmp import dircmp
 import os
 from pathlib import Path
@@ -8,6 +9,31 @@ import subprocess
 import shutil
 import sys
 
+
+def get_diff_files(dcmp, top_level=False):
+    msg = []
+    for name in dcmp.diff_files:
+        l, r = Path(dcmp.left) / name, Path(dcmp.right) / name
+        print(
+            "\n".join(
+                difflib.unified_diff(
+                    l.read_text().splitlines(keepends=True),
+                    r.read_text().splitlines(keepends=True),
+                    fromfile=str(l),
+                    tofile=str(r),
+                )
+            )
+        )
+        msg.append(f"diff in: {l.parts[-3:]}")
+    if dcmp.left_only:
+        msg.append(f"left only: {dcmp.left_only}")
+    if dcmp.right_only:
+        msg.append(f"right only: {dcmp.right_only}")
+    for sub_dcmp in dcmp.subdirs.values():
+        msg.extend(get_diff_files(sub_dcmp))
+    if top_level:
+        print("\n".join(msg))
+    return msg
 
 
 def test_default_codeplug(tmp_path):
@@ -29,5 +55,7 @@ def test_default_codeplug(tmp_path):
     )
     for f in output_dir.glob("*.log"):
         f.rename(tmp_path / f.name)
-    dircmp(input_dir, output_dir)
+    dcmp = dircmp(input_dir.parent, output_dir, ignore=[".DS_Store"])
+    diff_files = get_diff_files(dcmp, top_level=True)
+    assert not diff_files
 
