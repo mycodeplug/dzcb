01d458f473925730be6c65a8633b548e872ce814
Masen Furer <m_github@0x26.net>
2021-02-28 05:02:35 -0800

    test_model: test rename grouplist
    A renamed grouplist reference should be pushed
    through to the channel

--- tests/test_model.py
+++ tests/test_model.py
@@ -68,27 +68,45 @@ def complex_codeplug():
         dzcb.model.Talkgroup("PC2", 0xE02, dzcb.model.ContactType.PRIVATE, timeslot=2),
         dzcb.model.Talkgroup("PC3", 0xE03, dzcb.model.ContactType.PRIVATE, timeslot=1),
     )
-    channels = (
-        dzcb.model.AnalogChannel("A1", "146.520", "6.0"),
-        dzcb.model.AnalogChannel("A2", "146.530", "6.0"),
-        dzcb.model.AnalogChannel("A3", "146.540", "6.0"),
-        dzcb.model.DigitalChannel("D1", "443.4375", "9", talkgroup=contacts[0]),
-        dzcb.model.DigitalChannel("D2", "443.4375", "9", talkgroup=contacts[1]),
-        dzcb.model.DigitalChannel("D3", "443.4375", "9", talkgroup=contacts[2]),
-        dzcb.model.DigitalChannel(
-            "DR1", "443.4375", "9", static_talkgroups=contacts[:]
-        ),
-        dzcb.model.DigitalChannel(
-            "DR2", "444.4375", "9", static_talkgroups=[contacts[0], contacts[1]]
-        ),
-        dzcb.model.DigitalChannel(
-            "DR3", "445.4375", "9", static_talkgroups=[contacts[3], contacts[4]]
-        ),
-    )
     grouplists = (
         dzcb.model.GroupList("GL_ALL", contacts[:]),
         dzcb.model.GroupList("GL_GRP", contacts[:3]),
         dzcb.model.GroupList("GL_PRV", contacts[3:]),
+    )
+    channels = (
+        dzcb.model.AnalogChannel("A1", "146.520", "6.0"),
+        dzcb.model.AnalogChannel("A2", "146.530", "6.0"),
+        dzcb.model.AnalogChannel("A3", "146.540", "6.0"),
+        dzcb.model.DigitalChannel(
+            "D1", "443.4375", "9", talkgroup=contacts[0], grouplist=grouplists[0]
+        ),
+        dzcb.model.DigitalChannel(
+            "D2", "443.4375", "9", talkgroup=contacts[1], grouplist=grouplists[1]
+        ),
+        dzcb.model.DigitalChannel(
+            "D3", "443.4375", "9", talkgroup=contacts[2], grouplist=grouplists[1]
+        ),
+        dzcb.model.DigitalChannel(
+            "DR1",
+            "443.4375",
+            "9",
+            static_talkgroups=contacts[:],
+            grouplist=grouplists[1],
+        ),
+        dzcb.model.DigitalChannel(
+            "DR2",
+            "444.4375",
+            "9",
+            static_talkgroups=[contacts[0], contacts[1]],
+            grouplist=grouplists[1],
+        ),
+        dzcb.model.DigitalChannel(
+            "DR3",
+            "445.4375",
+            "9",
+            static_talkgroups=[contacts[3], contacts[4]],
+            grouplist=grouplists[2],
+        ),
     )
     scanlists = (
         dzcb.model.ScanList("SL_ALL", channels[:]),
@@ -252,3 +270,14 @@ def test_Channel_exclude_replacements(complex_codeplug):
         "D2",
         "DigitalRepeater1",
     ]
+
+
+def test_Grouplist_replacements(complex_codeplug):
+    old = "GL_ALL"
+    new = "gl_all"
+    gl_replacements = complex_codeplug.filter(
+        include=dzcb.model.Ordering(channels=["D1"]),
+        replacements=dzcb.model.Replacements(grouplists=((old, new),)),
+    )
+    assert gl_replacements.channels[0].grouplist_name(complex_codeplug) == old
+    assert gl_replacements.channels[0].grouplist_name(gl_replacements) == new
