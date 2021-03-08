3e5217b84963367d51e9a7f27b915c96f2c78af0
Masen Furer <m_github@0x26.net>
2021-03-08 12:43:51 -0800

    codeplug/default: update generate.sh
    specify all output formats, since these are no longer
    the default.

--- codeplug/default/generate.sh
+++ codeplug/default/generate.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# This script generates the same codeplug as generate.py
+# by running dzcb as a command line program
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
+OUTPUT=${OUTPUT:-$DIR/../../OUTPUT/$(dirname "$DIR")}
 python -m dzcb \
     --pnwdigital \
     --seattledmr \
@@ -11,5 +14,9 @@ python -m dzcb \
     --repeaterbook-proximity-csv "$DIR/../../src/dzcb/data/repeaterbook_proximity_zones.csv" \
     --scanlists-json $DIR/scanlists.json \
     --replacements $DIR/replacements.csv \
-    --order $DIR/order.csv -- \
-$OUTPUT/default
+    --order $DIR/order.csv \
+    --anytone \
+    --dmrconfig-template \
+    --farnsworth-template-json \
+    --gb3gf \
+-- $OUTPUT
