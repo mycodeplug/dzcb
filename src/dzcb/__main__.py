4be4c8878cf7bf0bdd59c8fca36440fc58cc714a
Masen Furer <m_github@0x26.net>
2021-03-08 12:42:18 -0800

    dzcb.__main__: arguments for all output formats
    main program doesn't generate any codeplugs by default
    instead output formats must be specified on the command line

--- src/dzcb/__main__.py
+++ src/dzcb/__main__.py
@@ -6,8 +6,16 @@ see dzcb.recipe for codeplug generation routines
 
 import argparse
 
+import dzcb.anytone
+import dzcb.gb3gf
 import dzcb.recipe
 import dzcb.repeaterbook
+
+
+def is_specified(arg):
+    if arg is not None and not arg:
+        return True
+    return arg
 
 
 if __name__ == "__main__":
@@ -34,8 +42,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--repeaterbook-proximity-csv",
-        type=argparse.FileType("r"),
-        default=None,
+        nargs="*",
         help="Fetch repeaters within X distance of POIs defined in a CSV file",
     )
     parser.add_argument(
@@ -84,6 +91,44 @@ if __name__ == "__main__":
         nargs="*",
         help="Specify one or more CSV files with object name replacements",
     )
+    parser.add_argument(
+        "--anytone",
+        nargs="*",
+        help="Anytone radio+CPS versions to use when creating anytone CSV. "
+        "One codeplug per radio+CPS version specified will be generated in the 'anytone' "
+        "subdir of the output directory. If no radio+CPS versions are given use "
+        "default versions: ({})".format(
+            ", ".join(dzcb.anytone.DEFAULT_SUPPORTED_RADIOS)
+        ),
+    )
+    parser.add_argument(
+        "--dmrconfig-template",
+        "--dmrconfig",
+        nargs="*",
+        help="dmrconfig conf file(s) to use when creating dmrconfig codeplugs. "
+        "One codeplug per file specified will be generated in the 'dmrconfig' "
+        "subdir of the output directory. If no files are given, use "
+        "default template files",
+    )
+    parser.add_argument(
+        "--farnsworth-template-json",
+        "--farnsworth-template",
+        "--farnsworth",
+        "--editcp",
+        nargs="*",
+        help="JSON file(s) to use when creating farnsworth editcp codeplugs. "
+        "One codeplug per file specified will be generated in the 'editcp' "
+        "subfolder of the output directory. If no files are given, use the"
+        "default template files",
+    )
+    parser.add_argument(
+        "--gb3gf",
+        nargs="*",
+        help="Radio types to use when creating gb3gf CSV files. "
+        "One codeplug per radio type specified will be generated in the 'gb3gf' "
+        "subdir of the output directory. If no radio types are given use "
+        "default: ({})".format(", ".join(dzcb.gb3gf.DEFAULT_SUPPORTED_RADIOS)),
+    )
     parser.add_argument("outdir", help="Write code plug files to this directory")
     args = parser.parse_args()
 
@@ -101,8 +146,8 @@ if __name__ == "__main__":
         order=args.order,
         reverse_order=args.reverse_order,
         replacements=args.replacements,
-        output_anytone=True,
-        output_dmrconfig=args.dmrconfig_template or True,
-        output_farnsworth=args.farnsworth_template_json or True,
-        output_gb3gf=True,
+        output_anytone=is_specified(args.anytone),
+        output_dmrconfig=is_specified(args.dmrconfig_template),
+        output_farnsworth=is_specified(args.farnsworth_template_json),
+        output_gb3gf=is_specified(args.gb3gf),
     ).generate(output_dir=args.outdir)
