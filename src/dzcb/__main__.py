b1d6ce0360baa0d0938425f5433e02123f8b3deb
Masen Furer <m_github@0x26.net>
2021-03-07 23:16:58 -0800

    dzcb.__main__: call through to dzcb.recipe
    remove all complicated logic from __main__, now all
    it does is instantiate a CodeplugRecipe and call
    generate()

--- src/dzcb/__main__.py
+++ src/dzcb/__main__.py
@@ -1,83 +1,13 @@
 """
 dzcb main program
+
+see dzcb.recipe for codeplug generation routines
 """
 
 import argparse
-import json
-from importlib_resources import contents, files
-import logging
-from pathlib import Path
-import os
-import shutil
 
-from dzcb import __version__
-import dzcb.anytone
-import dzcb.data
-import dzcb.farnsworth
-import dzcb.gb3gf
-import dzcb.k7abd
-import dzcb.log
-import dzcb.model
-import dzcb.output.dmrconfig
-import dzcb.pnwdigital
+import dzcb.recipe
 import dzcb.repeaterbook
-import dzcb.seattledmr
-
-
-logger = logging.getLogger("dzcb")
-
-
-def append_dir_and_create(path, component=None):
-    new_path = path
-    if component:
-        new_path = new_path / component
-    new_path.mkdir(parents=True, exist_ok=True)
-    return new_path
-
-
-def cache_user_or_default_text(object_name, user_path, default_path, cache_dir):
-    """
-    Read text from a user-specified or default path for object_name.
-
-    Side-effects:
-      * Logging at info level, mentioning object_name and the path used
-      * Copying either the user_path or default_path to cache_dir
-
-    Return:
-        Text of file content or empty string if neither path or default are specified
-    """
-    if user_path is None:
-        if not default_path:
-            return ""
-        path = Path(default_path)
-        logger.info("Cache default %s: '%s'", object_name, path.absolute())
-    else:
-        path = Path(user_path)
-        logger.info("Cache user-specified %s: '%s'", object_name, path.absolute())
-    dest = cache_dir / path.name
-    shutil.copy(path, dest)
-    return dest.read_text()
-
-
-def cache_user_or_default_json(object_name, user_path, default_path, cache_dir):
-    """
-    Read JSON from a user-specified or default path for object_name.
-
-    Side-effects:
-      * Logging at info level, mentioning object_name and the path used
-      * Copying either the user_path or default_path to cache_dir
-
-    Return:
-        Python objects
-    """
-    return json.loads(
-        cache_user_or_default_text(
-            object_name=object_name,
-            user_path=user_path,
-            default_path=default_path,
-            cache_dir=cache_dir,
-        )
-    )
 
 
 if __name__ == "__main__":
@@ -169,152 +99,22 @@ if __name__ == "__main__":
     parser.add_argument("outdir", help="Write code plug files to this directory")
     args = parser.parse_args()
 
-    outdir = append_dir_and_create(Path(args.outdir))
-    dzcb.log.init_logging(log_path=outdir)
-    logger.info("dzcb %s outdir='%s'", __version__, outdir)
-
-    cache_dir = append_dir_and_create(outdir, "cache")
-    logger.debug("Using cache_dir: '%s'", cache_dir)
-
-    # get overall ordering objects first and raise any validation errors
-    ordering = {}
-    for oarg_name in ["include", "exclude", "order", "reverse_order"]:
-        ordering[oarg_name] = dzcb.model.Ordering()
-        for f in getattr(args, oarg_name) or tuple():
-            ordering[oarg_name] += dzcb.model.Ordering.from_csv(
-                cache_user_or_default_text(
-                    object_name=oarg_name,
-                    user_path=f,
-                    default_path=None,
-                    cache_dir=cache_dir,
-                ).splitlines()
-            )
-
-    replacements = dzcb.model.Replacements()
-    for f in args.replacements or tuple():
-        replacements += dzcb.model.Replacements.from_csv(
-            cache_user_or_default_text(
-                object_name="replacements",
-                user_path=f,
-                default_path=None,
-                cache_dir=cache_dir,
-            ).splitlines()
-        )
-
-    # fetch data from the internet
-    if args.repeaterbook_proximity_csv:
-        dzcb.repeaterbook.zones_to_k7abd(
-            input_csv=args.repeaterbook_proximity_csv,
-            output_dir=cache_dir,
-            states=args.repeaterbook_state
-            or dzcb.repeaterbook.REPEATERBOOK_DEFAULT_STATES,
-            name_format=args.repeaterbook_name_format,
-        )
-
-    if args.pnwdigital:
-        dzcb.pnwdigital.cache_repeaters(cache_dir)
-
-    if args.seattledmr:
-        dzcb.seattledmr.cache_repeaters(cache_dir)
-
-    if args.default_k7abd:
-        default_k7abd_path = files(dzcb.data) / "k7abd"
-        logger.info("Cache default k7abd zones from: '%s'", default_k7abd_path)
-        shutil.copytree(default_k7abd_path, cache_dir, dirs_exist_ok=True)
-
-    if args.k7abd:
-        # copy any additional CSV directories into the cache_dir
-        for abd_dir in args.k7abd:
-            logger.info("Cache k7abd zones from: '%s'", abd_dir)
-            shutil.copytree(abd_dir, cache_dir, dirs_exist_ok=True)
-
-    # load additional data files or defaults
-    scanlists = cache_user_or_default_json(
-        object_name="scanlists",
-        user_path=args.scanlists_json,
-        default_path=files(dzcb.data).joinpath("scanlists.json"),
-        cache_dir=cache_dir,
-    )
-
-    # create codeplug from a directory of k7abd CSVs
-    cp = (
-        dzcb.k7abd.Codeplug_from_k7abd(cache_dir)
-        .filter(replacements=replacements, **ordering)
-        .replace_scanlists(scanlists)
-    )
-    logger.info("Generated %s", cp)
-
-    # GB3GF CSV - Radioddity GD77/OpenGD77, TYT MD-380, MD-9600, Baofeng DM1801, RD-5R
-    # XXX: Only support OpenGD77 at the moment
-    gb3gf_outdir = append_dir_and_create(outdir, "gb3gf")
-    gd77_outdir = append_dir_and_create(gb3gf_outdir, "opengd77")
-    dzcb.gb3gf.Codeplug_to_gb3gf_opengd77_csv(
-        cp=cp,
-        output_dir=gd77_outdir,
-    )
-
-    # The following models use expand_static_talkgroups to create
-    # one channel per talkgroup / one zone per repeater
-    fw_cp = (
-        cp.expand_static_talkgroups()
-        .filter(replacements=replacements, **ordering)
-        .replace_scanlists(scanlists)
-    )
-    logger.info("Expand static talkgroups %s", fw_cp)
-
-    # Anytone 578/868/878 stock CPS CSV import files
-    anytone_outdir = append_dir_and_create(outdir, "anytone")
-    dzcb.anytone.Codeplug_to_anytone_csv(
-        cp=fw_cp,
-        output_dir=anytone_outdir,
-    )
-
-    # Farnsworth JSON - TYT et. al w/ Zone Import!
-    farnsworth_templates = []
-    if args.farnsworth_template_json is None:
-        # Iterate through all farnsworth templates, generating codeplug for each
-        for f in (files(dzcb.data) / "farnsworth").iterdir():
-            if not str(f).endswith(".json"):
-                continue
-            farnsworth_templates.append((f, f.name, f.open("r")))
-    else:
-        farnsworth_templates = [
-            (ftj, os.path.basename(ftj), open(ftj, "r"))
-            for ftj in args.farnsworth_template_json
-        ]
-
-    fw_outdir = append_dir_and_create(outdir, "editcp")
-    for ftj, fname, fh in farnsworth_templates:
-        outfile = fw_outdir / fname
-        outfile.write_text(
-            dzcb.farnsworth.Codeplug_to_json(
-                cp=fw_cp,
-                based_on=fh,
-            )
-        )
-        logger.info("Wrote '%s' based JSON to '%s'", ftj, outfile)
-
-    # dmrconfig textual output
-    dmrconfig_templates = []
-    if args.dmrconfig_template is None:
-        # Iterate through all dmrconfig templates, generating codeplug for each
-        for f in (files(dzcb.data) / "dmrconfig").iterdir():
-            if f.suffix != ".conf":
-                continue
-            dmrconfig_templates.append(f)
-    else:
-        dmrconfig_templates = [Path(dt) for dt in args.dmrconfig_template if dt.lower() != "-"]
-
-    dm_outdir = append_dir_and_create(outdir, "dmrconfig")
-    table = dzcb.output.dmrconfig.Table(
-        codeplug=fw_cp,
-    )
-    for dt in dmrconfig_templates:
-        outfile = dm_outdir / dt.name
-        outfile.write_text(
-            dzcb.output.dmrconfig.Dmrconfig_Codeplug(
-                table,
-                template=dt.read_text(),
-            ).render_template()
-        )
-        logger.info("Wrote dmrconfig template '%s' to '%s'", dt.name, outfile)
+    dzcb.recipe.CodeplugRecipe(
+        source_pnwdigital=args.pnwdigital,
+        source_seattledmr=args.seattledmr,
+        source_default_k7abd=args.default_k7abd,
+        source_k7abd=args.k7abd,
+        source_repeaterbook_proximity=args.repeaterbook_proximity_csv,
+        repeaterbook_states=args.repeaterbook_state,
+        repeaterbook_name_format=args.repeaterbook_name_format,
+        scanlists_json=args.scanlists_json,
+        include=args.include,
+        exclude=args.exclude,
+        order=args.order,
+        reverse_order=args.reverse_order,
+        replacements=args.replacements,
+        output_anytone=True,
+        output_dmrconfig=args.dmrconfig_template or True,
+        output_farnsworth=args.farnsworth_template_json or True,
+        output_gb3gf=True,
+    ).generate(output_dir=args.outdir)
