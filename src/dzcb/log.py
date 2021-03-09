e759298bc294cf35e7629be072f47bd92bdf6c5a
Masen Furer <m_github@0x26.net>
2021-03-08 20:38:25 -0800

    dzcb.log: deinit_logging to clean up after a run
    allow the logging handlers to be removed and reassigned

--- src/dzcb/log.py
+++ src/dzcb/log.py
@@ -4,8 +4,10 @@ import time
 
 logger = logging.getLogger(__name__)
 LOG_FORMAT = "%(asctime)s [%(name)s] [%(levelname)s]  %(message)s"
-initialized = False
-file_handler = False
+file_handler = None
+stream_handler = None
+
+root_logger = logging.getLogger()
 
 
 def change_log_level(to):
@@ -33,36 +35,67 @@ def keep_to_delimiter(d):
     return _keep_to_delimiter
 
 
+def deinit_stream_handler():
+    global stream_handler
+    if stream_handler is not None:
+        root_logger.removeHandler(stream_handler)
+        stream_handler = None
+
+
+def deinit_file_handler():
+    global file_handler
+    if file_handler is not None:
+        root_logger.removeHandler(file_handler)
+        file_handler = None
+        redirect_warnings(remove=True)
+
+
+warning_filters = (
+    change_log_level(to=logging.DEBUG),
+    ltrim_to_delimiter(": "),
+    keep_to_delimiter("\n"),
+)
+
+
+def redirect_warnings(remove=False):
+    warning_logger = logging.getLogger("py.warnings")
+    adjustFilter = warning_logger.addFilter
+    if remove:
+        adjustFilter = warning_logger.removeFilter
+    for f in warning_filters:
+        adjustFilter(filter=f)
+    logging.captureWarnings(not remove)
+
+
 def init_logging(log_path=None):
-    global initialized, file_handler
+    global file_handler, stream_handler
 
-    root = logging.getLogger()
+    if stream_handler is not None:
+        deinit_stream_handler()
 
-    if not initialized:
-        formatter = logging.Formatter(LOG_FORMAT)
-        root.setLevel(logging.NOTSET)
+    formatter = logging.Formatter(LOG_FORMAT)
+    root_logger.setLevel(logging.NOTSET)
 
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
-        stream_handler.setLevel(logging.INFO)
-        root.addHandler(stream_handler)
-        initialized = True
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.INFO)
+    root_logger.addHandler(stream_handler)
 
     if log_path is not None:
         if file_handler is not None:
-            root.removeHandler(file_handler)
-            file_handler = None
+            deinit_file_handler()
         filename = time.strftime("dzcb.%Y_%m_%d_%H%M%S.log")
         path = Path(log_path) / filename
         logger.info("Logging to '%s' at DEBUG level", path)
         file_handler = logging.FileHandler(path)
         file_handler.setFormatter(formatter)
         file_handler.setLevel(logging.DEBUG)
-        root.addHandler(file_handler)
+        root_logger.addHandler(file_handler)
 
         # if a file was specified, spew warnings there instead of the console
-        warning_logger = logging.getLogger("py.warnings")
-        warning_logger.addFilter(filter=change_log_level(to=logging.DEBUG))
-        warning_logger.addFilter(filter=ltrim_to_delimiter(": "))
-        warning_logger.addFilter(filter=keep_to_delimiter("\n"))
-        logging.captureWarnings(True)
+        redirect_warnings()
+
+
+def deinit_logging():
+    deinit_stream_handler()
+    deinit_file_handler()
